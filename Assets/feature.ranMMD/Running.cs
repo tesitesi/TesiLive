6aff6041b020161c52b3486b4cf98ea892ee5b3f
tesiyosi <jo4etf@gmail.com>
2019-03-21 13:31:41 +0900

    First Commit

---
+++ Assets/feature.ranMMD/Running.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+
+    Rigidbody rb;
+    //Animatorを入れる変数
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+    }
+}
